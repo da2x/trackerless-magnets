462051e5db968fbf583d5406d21ae62a7f8435ab
Daniel Aleksandersen <code@daniel.priv.no>
2016-08-02 13:21:25 +0200

    Delete legacy Mozilla Add-on SDK code

--- mozilla-add-on-sdk/index.js
+++
@@ -1,8 +0,0 @@
-var data = require('sdk/self').data,
-    pageMod = require('sdk/page-mod');
-
-pageMod.PageMod({
-    include: '*',
-    contentScriptFile: data.url('content-script.js'),
-    contentScriptWhen: 'ready'
-});
