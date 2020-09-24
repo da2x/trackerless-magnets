aee42946cbc72b0120e43e55c5efd74a9b16cd64
Daniel Aleksandersen <code@daniel.priv.no>
2020-09-24 15:33:37 +0200

    Fix BitTorrent multihash/multiple xt

--- webextensions/data/content-script.js
+++ webextensions/data/content-script.js
@@ -8,19 +8,24 @@ for (link in magnetlinks)
 
   var old_url = new URL(magnetlinks[link].href);
 
-  // btih (required)
+  // xt hashes (required)
   if (old_url.searchParams.has('xt'))
   {
-    var new_url = `magnet:?xt=${old_url.searchParams.getAll('xt')}`;
+    var new_url = 'magnet:?'
+
+    // copy hashes
+    old_url.searchParams.getAll('xt').forEach(
+      xt => new_url += `&xt=${xt}`
+    );
 
     // content length (optional)
     if (old_url.searchParams.has('xl'))
-      new_url += `&xl=${old_url.searchParams.getAll('xl')}`;
+      new_url += `&xl=${old_url.searchParams.getAll('xl')[0]}`;
 
     // file name (optional)
     if (old_url.searchParams.has('dn'))
-      new_url += `&dn=${old_url.searchParams.getAll('dn')}`;
+      new_url += `&dn=${old_url.searchParams.getAll('dn')[0]}`;
 
-    magnetlinks[link].href = new_url;
+    magnetlinks[link].href = new_url.replace('?&', '?');
   }
 }
